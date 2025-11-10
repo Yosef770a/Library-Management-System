1fa210a3f4727ca7bae6f83795abe7123878c8c6
Yosef770a <moked7700@gmail.com>
2025-11-10 09:35:51 +0200

    User class completed

---
+++ classes/book.py
@@ -0,0 +1,10 @@
+class Book:
+    def __init__(self, title: str, author:str, ISBN:int):
+        self.title = title
+        self.author = author
+        self.ISBN = ISBN
+        self.is_available = True
+
+
+    def __str__(self):
+        return f"title: {self.title},author: {self.author}, {"available" if self.is_available else "not available"}"
