40cbc826515619cf8121ad0083547ee4b34f7598
Yosef770a <moked7700@gmail.com>
2025-11-10 12:35:51 +0200

    Adding borrow_book method

--- classes/library.py
+++ classes/library.py
@@ -1,3 +1,4 @@
+
 class Library:
     def __init__(self):
         self.list_of_books=[]
@@ -6,5 +7,21 @@ class Library:
     def __str__(self):
         return f"  books {self.list_of_books}, users {self.list_of_users}"
 
-    
-         
+
+    def borrow_book(self, user_id, book_isbn):
+        for book in self.list_of_books:
+            if book.ISBN == book_isbn:
+                if book.is_available:
+                    for user in self.list_of_users:
+                        if user.id == user_id:
+                            user.borrowed_books.append(book)
+                            book.is_available = False
+                            return f"The book has been successfully loaned to you {user.name}"
+                    return "User not found"
+                else:
+                    return "The requested book is not available"
+        return "Book does not exist"
+
+
+
+
