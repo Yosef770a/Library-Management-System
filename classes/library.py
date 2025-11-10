e48b9a843d31812a1ddaa82dbc0c50295de64016
gordonzev914-lang <gordonzev914@gmail.com>
2025-11-10 11:00:29 +0200

    first 2 methods

--- classes/library.py
+++ classes/library.py
@@ -1,3 +1,5 @@
+from user import User
+from book import Book
 class Library:
     def __init__(self):
         self.list_of_books=[]
@@ -6,5 +8,17 @@ class Library:
     def __str__(self):
         return f"  books {self.list_of_books}, users {self.list_of_users}"
 
+    def add_book(self,book):
+        self.list_of_books.append(book)
+        return self.list_of_books
     
-         
+
+    def add_user(self,user):
+        self.list_of_users.append(user)
+        return self.list_of_users
+    
+    
+        
+
+
+      
