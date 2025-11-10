85226fcdb0187d000ca778ffd8121f6559837f5a
Yosef770a <moked7700@gmail.com>
2025-11-10 12:41:14 +0200

    Adding return_book method

--- classes/library.py
+++ classes/library.py
@@ -23,5 +23,18 @@ class Library:
         return "Book does not exist"
 
 
+    def return_book(self, user_id, book_isbn):
+        for book in self.list_of_books:
+            if book.ISBN == book_isbn:
+                if not(book.is_available):
+                    for user in self.list_of_users:
+                        if user.id == user_id:
+                            user.borrowed_books.remove(book)
+                            book.is_available = True
+                            return f"The book was successfully returned to the library"
+                    return "User not found"
+                else:
+                    return "The book is already available"
+        return "Book does not exist"
 
 
