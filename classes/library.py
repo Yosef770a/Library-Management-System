4bf29fe510c494ed9aa1928e4abef032f3aa6c3c
Yosef770a <moked7700@gmail.com>
2025-11-11 09:19:47 +0200

    Handling edge cases and adding prints to the user

--- classes/library.py
+++ classes/library.py
@@ -8,23 +8,30 @@ class Library:
         return f"books {self.list_of_books},users {self.list_of_users},"
 
     def add_book(self,book):
+        for bookf in self.list_of_books:
+            if book.ISBN == bookf.ISBN:
+                return "\nThe book already exists in the library"
         self.list_of_books.append(book)
-        return self.list_of_books
+        return "\nThe book has been successfully added to the library"
     
     def add_user(self,user):
+        for userf in self.list_of_users:
+            if user.id == userf.id:
+                return "\nThe subscription already exists in the system" 
         self.list_of_users.append(user)
-        return self.list_of_users
+        return "\nThe subscription has been successfully added to the system"
+                
     
     def list_available_books(self):
-        self.list_of_available_books=[]
+        list_of_available_books=[]
         for book in self.list_of_books:
-            if book not in self.list_of_available_books:
-                if book.is_available==True:
-                    self.list_of_available_books.append(book)               
+            if book not in list_of_available_books:
+                if book.is_available:
+                    list_of_available_books.append(book)               
             else:
-                if book.is_available==False:
-                    self.list_of_available_books.pop(book)
-        return self.list_of_available_books
+                if not book.is_available:
+                    list_of_available_books.pop(book)
+        return list_of_available_books
 
 
     def search_book(self,search_content):
@@ -44,7 +51,7 @@ class Library:
                         if str(user.id) == user_id:
                             user.borrowed_books.append(book)
                             book.is_available = False
-                            return f"The book has been successfully loaned to you {user.name}"
+                            return f"\n The book was successfully loaned to user {user.name}"
                     return "User not found"
                 else:
                     return "The requested book is not available"
@@ -53,12 +60,10 @@ class Library:
 
     def return_book(self, user_id, book_isbn):
         for book in self.list_of_books:
-            print(self.list_of_books[0])
-            if book.ISBN == book_isbn:
-                print(self.list_of_books[0])
+            if str(book.ISBN) == book_isbn:
                 if not(book.is_available):
                     for user in self.list_of_users:
-                        if user.id == user_id:
+                        if str(user.id) == user_id:
                             user.borrowed_books.remove(book)
                             book.is_available = True
                             return f"The book was successfully returned to the library"
--- classes/menu.py
+++ classes/menu.py
@@ -17,30 +17,38 @@ def menu():
               """) 
         choice = input("Enter your choice: ")    
         if choice == "1": 
-            user_name = input("Please enter the subscriber name: ")
+            user_name = input("\nPlease enter the subscriber name: ")
             user_id = input("please enter the subscriber id number: ")
-            library.add_user(User(user_name, user_id))
+            print(library.add_user(User(user_name, user_id)))
 
         elif choice == "2":
-             ISBN = input("Enter ISBN: ")
+             ISBN = input("\nPlease enter ISBN: ")
              title = input("Enter the book title: ")
              author = input("Enter the author's name: ")
              library.add_book(Book(ISBN, title, author))
             
         elif choice == "3":
-            user_id=input("please enter the subscriber id number: ")
+            user_id=input("\nplease enter the subscriber id number: ")
             book_number=input("Enter the book's ISBN: ")
             print(library.borrow_book(user_id, book_number))
 
         elif choice == "4":
-            user_id=int(input("please enter the subscriber id number: "))
-            book_number=int(input("Enter the book's ISBN: "))
+            user_id=input("\nplease enter the subscriber id number: ")
+            book_number=input("Enter the book's ISBN: ")
             print(library.return_book(user_id,book_number))
 
         elif choice=="5":
-            library.list_available_books()
+            if library.list_available_books():
+                print(f"\nThere are {len(library.list_available_books())} books available\n")
+                for i,book in enumerate(library.list_available_books()):
+                    print(f"{i+1}. ISBN: {book.ISBN}, Title: {book.title}, Author: {book.author}")
+                print("\nEnd of list")
+            else:
+                print("\nNo available books found")
+                
 
         elif choice=="6":
-            search_content = input("Please enter the name or title of the book to search for: ")
+            search_content = input("\nPlease enter the name or title of the book to search for: ")
             library.search_book(search_content)
 
+menu()
